6ac7029534bfb051ab828d8eb63724140d26cfaf
Pedro Henrique Martins Garcia <pedro.phmg@gmail.com>
2019-09-13 16:39:33 -0300

    Append person to csv

--- UpdateCSV.py
+++ UpdateCSV.py
@@ -1,10 +1,24 @@
-def updateCsv(fileName, passengerId, pClass, name, sex, age, sibSp, parch, ticket, fare, cabin, embarked):
-    inputFile = open(fileName, "a")    # OppeningFile
-    inputFile.write("Primeira,Segunda\n")   # Appending Lines
+import csv
+import os
+#import pandas as pd
 
-# Test Procedure
-updateCsv("TestCSV.csv","","","","","","","","","","","")
+#def appendNewPerson(fileName, passengerId, pClass, name, sex, age, sibSp, parch, ticket, fare, cabin, embarked):
+#    pd.read_csv(fileName)
 
-# Test Verification
-f = open("TestCSV.csv", "r")
-print(f.read())
+# This function will append a line to a CSV File
+def appendNewPerson(fileName, passengerId, pClass, name, sex, age, sibSp, parch, ticket, fare, cabin, embarked):
+    inputFile = open(fileName, "a")    # Oppening File
+    inputFile.write(passengerId + "," + pClass + "," + name + "," + sex + "," + age + "," + sibSp + "," + parch + "," + ticket + "," + fare + "," + cabin + "," + embarked)   # Appending Lines
+    inputFile.write("\n")
+    #inputFile.write("Primeira{},Segunda{}\n".format("3","3"))   # Appending Lines
+    inputFile.close()
+
+# This function will update a field in a CSV file
+#def updateCsv(fileName, rowIndex, coloumnIndex, ):
+
+
+# Test
+#appendNewPerson("TestCSV.csv","passengerId","pClass","name","sex","age","sibSp","parch","ticket","fare","cabin","embarked")
+#f = open("TestCSV.csv", "r")
+#print(f.read())
+#f.close()
